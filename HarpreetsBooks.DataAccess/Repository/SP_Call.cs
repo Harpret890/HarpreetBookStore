ae684e7986210dccc8fc65d750cc7765ad3b8870
W0802461 <W0802461@myscc.ca>
2022-11-11 17:46:39 -0500

    updated the SP_Call.cs page

--- HarpreetsBooks.DataAccess/Repository/SP_Call.cs
+++ HarpreetsBooks.DataAccess/Repository/SP_Call.cs
@@ -10,36 +10,67 @@ namespace HarpreetsBooks.DataAccess.Repository
 {
     public class SP_Call : ISP_Call
     {
+        // access the database 
         private readonly ApplicationDbContext _db;
-        private static string ConnectionString = ""; // needed to called the stored procedures
+        private static string ConnectionString = "";  // needed to called the stored procedures 
 
-        // constructor to open a SQL connectiom 
+        // constructor to open a SQL connection 
         public SP_Call(ApplicationDbContext db)
         {
             _db = db;
             ConnectionString = db.Database.GetDbConnection().ConnectionString;
         }
-        //  implements the ISP_Call interface
+        
+        // implements the ISP_Call interface 
         public void Dispose()
         {
             _db.Dispose();
         }
+
         public void Execute(string procedureName, DynamicParameters param = null)
         {
-            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            using(SqlConnection sqlCon = new SqlConnection(ConnectionString))
             {
                 sqlCon.Open();
                 sqlCon.Execute(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
             }
         }
-        public IEnumerable<T> List<T>(string procedureName sqlCon = new SqlConnection(ConnectionString))
-        {
-            sqlCon.Open();
-            return sqlCon.Query<T>(procedureName, ParamArrayAttribute, commandType: System.Data.CommandType.StoredProcedure);
-        }
-        public Tuple<IEnumerable<T1>,IEnumerable<T2>> List<T1,T2>(string procedureName, DynamicParameters param = null)
-        {
 
+        public IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                var result = SqlMapper.QueryMultiple(sqlCon, procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+                var item1 = result.Read<T1>().ToList();
+                var item2 = result.Read<T2>().ToList();
+
+                if(item1 != null && item2 != null)
+                {
+                    return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(item1, item2);
+                }
+            }
+            return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(item1, item2);
+        }
+
+        public T OneRecord<T>(string procedurename, DynamicParameters Param = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Single<T>(string procedurename, DynamicParameters param = null)
+        {
+            throw new NotImplementedException();
         }
     }
+
 }
